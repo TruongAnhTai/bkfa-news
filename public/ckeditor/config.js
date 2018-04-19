08d5a268a361f89402e58f7fd667d11e2d75ff76
Tran_Binh <tranbinhbak@gmail.com>
2018-04-19 23:44:05 +0700

    insert ckeditor

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -1,10 +1,6 @@
-/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
- */
-
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function(config) {
+    config.language = 'vi';
+    config.uiColor = '#F1F1F1';
+    config.height = 300;
+    config.toolbarCanCollapse = true;
 };
